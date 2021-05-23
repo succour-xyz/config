05d2035b2a73e0298714ebc4906ad9c6f54413dd
Piyush Mehta <me@piyushmehta.com>
2021-05-23 22:53:06 +0530

    ts-doc

--- packages/eslint-config/index.js
+++ packages/eslint-config/index.js
@@ -1,20 +1,23 @@
 module.exports = {
-  "extends": [
-  "plugin:@typescript-eslint/recommended",
-  "prettier",
-  "plugin:prettier/recommended",
-],
-  "parser": "@typescript-eslint/parser",
-  "plugins": ["@typescript-eslint", "prettier"],
-  "rules": {
-  "prettier/prettier": "warn"
-},
-  "overrides": [
-  {
-    "files": ["src/**/*.ts"],
-    "parserOptions": {
-      "project": ["./tsconfig.json"]
-    }
-  }
-]
-}
+  extends: [
+    "plugin:@typescript-eslint/recommended",
+    "prettier",
+    "plugin:prettier/recommended",
+  ],
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint", "prettier", "eslint-plugin-tsdoc"],
+  rules: {
+    "prettier/prettier": "warn",
+  },
+  overrides: [
+    {
+      files: ["src/**/*.ts"],
+      parserOptions: {
+        project: ["./tsconfig.json"],
+      },
+    },
+  ],
+  rules: {
+    "tsdoc/syntax": "warn",
+  },
+};
